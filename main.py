4b43eca0e8ae08a19dbd17a511bab35146dc60e9
Sarah Alves Chagas <90582944+sarahalvesc@users.noreply.github.com>
2024-06-11 19:27:39 -0300

    Subindo o restante do código

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 # Criação do modelo
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
 # Avaliação de métricas
 from sklearn.metrics import accuracy_score
@@ -24,7 +25,6 @@ df.replace('Torgersen', '2', inplace=True)
 novas_colunas = ['island', 'sex', 'culmen_length_mm', 'culmen_depth_mm', 'flipper_length_mm', 'body_mass_g', 'species']
 df_reindexado = df.reindex(columns=novas_colunas)
 
-
 # instanciando modelos
 tree = DecisionTreeClassifier()
 
@@ -37,7 +37,7 @@ y = df.loc[:, ['species']]
 y = np.array(y)
 
 # Separando treino e teste
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 print(f"Tamanho X de treino: {X_train.shape}")
 print(f"Tamanho X de teste: {X_test.shape}")
 print(f"Tamanho y de treino: {y_train.shape}")
@@ -54,3 +54,30 @@ print()
 tree_score = accuracy_score(y_test, tree_predict)
 print(f"Pontuação Decision Tree:{tree_score}")
 print()
+
+predict = tree.predict(X_test)
+
+relatorio_classificacao = classification_report(y_test, predict, target_names=['Adelie', 'Chinstrap', 'Gentoo'])
+print("Relatório de Classificação:\n", relatorio_classificacao)
+
+def classificar_novos_dados(tree):
+    print("Insira os seguintes dados para classificação:")
+    island = int(input("Ilha (0 para Biscoe, 1 para Dream, 2 para Torgersen): "))
+    sex = int(input("Sexo (0 para FEMALE, 1 para MALE): "))
+    culmen_length_mm = float(input("Comprimento do culmen (mm): "))
+    culmen_depth_mm = float(input("Profundidade do culmen (mm): "))
+    flipper_length_mm = float(input("Comprimento da nadadeira (mm): "))
+    body_mass_g = float(input("Massa corporal (g): "))
+
+    # Criar um DataFrame com os dados inseridos
+    novos_dados = pd.DataFrame([[island, sex, culmen_length_mm, culmen_depth_mm, flipper_length_mm, body_mass_g]],
+                               columns=['island', 'sex', 'culmen_length_mm', 'culmen_depth_mm', 'flipper_length_mm',
+                                        'body_mass_g'])
+
+    predicao = tree.predict(novos_dados)
+    especies = {0: 'Adelie', 1: 'Chinstrap', 2: 'Gentoo'}
+
+    print(f"A previsão do modelo é que o pinguim pertence à espécie: {especies[predicao[0]]}")
+
+
+classificar_novos_dados(tree)
